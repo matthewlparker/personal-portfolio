d5781ea85c33825631fc089b38de07378ded95c6
Matthew Parker <cadburylion@outlook.com>
2017-09-11 13:37:41 -0700

    added image assets folder, image not displaying

--- src/component/projects/index.js
+++ src/component/projects/index.js
@@ -3,8 +3,13 @@ import React from 'react'
 class Projects extends React.Component{
   render() {
     return(
-      <div className = "projects">
+      <div className = 'projects'>
         <h3> My projects go here. </h3>
+
+        <div className="project">
+          <h4> Guardians of the Goats </h4>
+          <img src='../../assets/goat-shadow.png' alt='goat-icon' className='gog-icon' />
+        </div>
       </div>
     )
   }
