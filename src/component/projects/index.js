2102ba61d6866c0e0e7856370ac3af45932810ca
Matthew Parker <cadburylion@outlook.com>
2017-09-13 10:16:07 -0700

    refactored projects styles to use li elements with working delight

--- src/component/projects/index.js
+++ src/component/projects/index.js
@@ -4,58 +4,34 @@ import React from 'react'
 class Projects extends React.Component{
   render() {
     return(
-      <div className = 'projects'>
+      <section className='featured-work'>
 
-        <a href='https://gourmet-swap-frontend.herokuapp.com/landing' target='_blank'>
-        <div className='project' >
-            <img src={require('../../assets/gourmet-swap.png')} alt='gourmet swap icon' className='gs-icon' />
-            <h5 className='title'> Gourmet Swap </h5>
-
-
-            </div>
+        <ul className='projects'>
+          <li className='gourmet-swap'>
+            <a href='https://gourmet-swap-frontend.herokuapp.com/landing' target='_blank'>
+              <img src={require('../../assets/gourmet-swap.png')} alt='gourmet swap icon' className='gs-icon' />
+              <h3 className='title'> Gourmet Swap </h3>
             </a>
-
-          <a href='https://seattle-reads.herokuapp.com/' target='_blank'>
-          <div className='project' >
-            <img src={require('../../assets/seattle-reads.png')} alt='graph of library checkouts' className='sr-icon' />
-            <h5 className='title'> Seattle Reads </h5>
-
-          </div>
-          </a>
-
-          <a href='https://cadburylion.github.io/' target='_blank'>
-          <div className='project' >
-            <img src={require('../../assets/goat-shadow.png')} alt='goat-icon' className='gog-icon' />
-            <h5 className='title'> Guardians of the Goats </h5>
             <h4> Description </h4>
-          </div>
-          </a>
+          </li>
 
-          <a href='https://seattle-reads.herokuapp.com/' target='_blank'>
-          <div className='project' >
-            <img src={require('../../assets/seattle-reads.png')} alt='graph of library checkouts' className='sr-icon' />
-            <h5 className='title'> Seattle Reads </h5>
+          <li className='seattle-reads'>
+            <a href='https://seattle-reads.herokuapp.com/' target='_blank'>
+              <img src={require('../../assets/seattle-reads.png')} alt='graph of library checkouts' className='sr-icon' />
+              <h3 className='title'> Seattle Reads </h3>
+            </a>
+            <h4> Description </h4>
+          </li>
 
-          </div>
-          </a>
-
-          <a href='https://seattle-reads.herokuapp.com/' target='_blank'>
-          <div className='project' >
-            <img src={require('../../assets/seattle-reads.png')} alt='graph of library checkouts' className='sr-icon' />
-            <h5 className='title'> Seattle Reads </h5>
-
-          </div>
-          </a>
-
-          <a href='https://seattle-reads.herokuapp.com/' target='_blank'>
-          <div className='project' >
-            <img src={require('../../assets/seattle-reads.png')} alt='graph of library checkouts' className='sr-icon' />
-            <h5 className='title'> Seattle Reads </h5>
-
-          </div>
-          </a>
-
-      </div>
+          <li className='guardians'>
+            <a href='https://cadburylion.github.io/' target='_blank'>
+              <img src={require('../../assets/goat-shadow.png')} alt='goat-icon' className='gog-icon' />
+              <h3 className='title'> Guardians of the Goats </h3>
+            </a>
+            <h4> Description </h4>
+          </li>
+        </ul>
+      </section>
     )
   }
 }
