c1fe3809f97590111ede9815679442e12cef6721
Matthew Parker <cadburylion@outlook.com>
2017-09-15 15:26:25 -0700

    added avatar image to assets and styled about intro

--- src/component/about/index.js
+++ src/component/about/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import './styles.scss'
 
 export default class About extends React.Component{
   render() {
     return(
-      <div className = "projects">
-        <h3> {'Hi, I\'m Matthew.'} </h3>
-        <p> {'I\'m a Full-Stack JavaScript developer and graduate of Code Fellows in Seattle.'} </p>
-        <p> {'There I learned how to write clean, modern code using accepted best-practices. I specialize in React, and love learning new technologies!'} </p>
-      </div>
+      <main className='about-main'>
+
+        <div className='about-intro'>
+          <h1> About Matthew </h1>
+          <h2> Building high-quality applications and exceptional user experiences </h2>
+        </div>
+
+        <div className='about-content'>
+          <div className='about-avatar'>
+            <img src={require('../../assets/matt-profile.png')} />
+          </div>
+        </div>
+      </main>
     )
   }
 }
