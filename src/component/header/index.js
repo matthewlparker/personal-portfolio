6db32165ef215d5e028397f4e10791842eab3501
Matthew Parker <cadburylion@outlook.com>
2017-09-17 10:39:29 -0700

    Refactored nav element to render based on state

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -5,27 +5,31 @@ import * as route from '../../action/route.js'
 import {BrowserRouter, Link} from 'react-router-dom'
 
 import './styles.scss'
-
 import Home from '../home'
-
-
 
 
 class Header extends React.Component{
   constructor(props){
     super(props)
-    this.state = {
-      route: this.props.route || '/' ,
-    }
   }
 
   componentWillMount(){
-    this.setState({route: document.location.href.split('8080')[1]})
+    let pathname = document.location.href.split('com')[1]
 
+    if(pathname === '/about'){
+      this.props.routeToAbout()
+    } else if(pathname === '/contact'){
+      this.props.routeToContact()
+    } else if(pathname){
+      this.props.routeToPortfolio()
+    }
   }
 
   render() {
-    let pathname = document.location.href.split('8080')[1]
+    let aboutClass = this.props.route === '/about' ? 'about current' : 'about'
+    let portfolioClass = this.props.route === '/' ? 'portfolio current' : 'portfolio'
+    let contactClass = this.props.route === '/contact' ? 'contact current' : 'contact'
+
     return(
 
       <div className='header-field'>
@@ -39,42 +43,19 @@ class Header extends React.Component{
           </div>
         </Link>
 
-
         <nav className='nav-field'>
-
           <ul>
-
             <li>
-              {util.renderIf(pathname === '/about',
-                <Link to='/about' className='about current' onClick={this.props.routeToAbout}> About </Link>
-              )}
-              {util.renderIf(pathname !== '/about',
-                <Link to='/about' className='about' onClick={this.props.routeToAbout}> About </Link>
-              )}
+                <Link to='/about' className={aboutClass} onClick={this.props.routeToAbout}> About </Link>
             </li>
-
             <li>
-              {util.renderIf(pathname === '/',
-                <Link to='/' className='portfolio current' onClick={this.props.routePortfolio}> Portfolio </Link>
-              )}
-              {util.renderIf(pathname !== '/',
-                <Link to='/' className='portfolio' onClick={this.props.routeToPortfolio}> Portfolio </Link>
-              )}
+                <Link to='/' className={portfolioClass} onClick={this.props.routeToPortfolio}> Portfolio </Link>
             </li>
-
             <li>
-              {util.renderIf(pathname === '/contact',
-                <Link to='/contact' className='contact current' onClick={this.props.routeToContact}> Contact </Link>
-              )}
-              {util.renderIf(pathname !== '/contact',
-                <Link to='/contact' className='contact' onClick={this.props.routeToContact}> Contact </Link>
-              )}
+                <Link to='/contact' className={contactClass} onClick={this.props.routeToContact}> Contact </Link>
             </li>
-
           </ul>
-
         </nav>
-
       </div>
     )
   }
