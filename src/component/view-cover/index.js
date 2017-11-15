fe6cbf04d35e2c54ddf970307ce6d49b4dbd91bd
Matthew Parker <cadburylion@outlook.com>
2017-11-15 13:39:01 -0800

    Added basic media styles to home

--- src/component/view-cover/index.js
+++ src/component/view-cover/index.js
@@ -14,16 +14,42 @@ class ViewCover extends React.Component{
   handleCover(){
     this.props.handleCover('COVER_TOGGLE')
   }
+  //
+  // browserSpecificCode(){
+  //   //Check if browser is IE
+  //   if (navigator.userAgent.search("MSIE") & gt; = 0) {
+  //       // insert conditional IE code here
+  //   }
+  //   //Check if browser is Chrome
+  //   else if (navigator.userAgent.search("Chrome") & gt; = 0) {
+  //       // insert conditional Chrome code here
+  //   }
+  //   //Check if browser is Firefox
+  //   else if (navigator.userAgent.search("Firefox") & gt; = 0) {
+  //       // insert conditional Firefox Code here
+  //   }
+  //   //Check if browser is Safari
+  //   else if (navigator.userAgent.search("Safari") & gt; = 0 & amp; & amp; navigator.userAgent.search("Chrome") & lt; 0) {
+  //       // insert conditional Safari code here
+  //   }
+  //   //Check if browser is Opera
+  //   else if (navigator.userAgent.search("Opera") & gt; = 0) {
+  //       // insert conditional Opera code here
+  //   }
+  //
+  // }
 
   render(){
-    console.log('view-cover-render')
+
+    console.log(navigator.userAgent.search('Firefox') > 0)
+
     let arrowIconClass = (this.props.coverOpen && this.props.lightTheme) ? 'view-cover-toggle-icon-open-light'
     : (this.props.coverOpen && !this.props.lightTheme) ? 'view-cover-toggle-icon-open-dark'
     : (!this.props.coverOpen && this.props.lightTheme) ? 'view-cover-toggle-icon-closed-light'
     : (!this.props.coverOpen && !this.props.lightTheme) ? 'view-cover-toggle-icon-closed-dark'
     : ''
     return (
-      <div className={`view-cover-main ${this.props.coverOpen ? 'view-cover-main-open' : ''}`} onClick={this.handleCover}>
+      <div className={`view-cover-main ${this.props.coverOpen ? 'view-cover-main-open' : ''}`} onClick={this.handleCover} >
 
 
         <div className='view-cover-content' >
@@ -38,14 +64,16 @@ class ViewCover extends React.Component{
           <p className='view-cover-p4'>things</p>
 
           {!this.props.lightTheme ?
-            <video className='background-video' loop muted autoPlay alt='video loop of stars'>
-              <source src='https://d1235ca2z646oc.cloudfront.net/videos/processed/255/MadeInLadakh-HD_2.mp4.mp4' type='video/mp4' onClick={this.handleCover} />
-              Your browser does not support the video tag.
-            </video>
+
+              <video className='background-video' loop muted autoPlay alt='video loop of stars'>
+                <source src='https://d1235ca2z646oc.cloudfront.net/videos/processed/255/MadeInLadakh-HD_2.mp4.mp4' type='video/mp4' />
+                Your browser does not support the video tag.
+              </video>
+
             : undefined
           }
           {this.props.lightTheme ?
-            <video className='background-video' loop muted autoPlay alt='video loop of stars'>
+            <video className='background-video' loop muted autoPlay alt='video loop of clouds'>
               <source src='https://www.videvo.net/videvo_files/converted/2014_06/preview/Blue_Sky_and_Clouds_Timelapse_0892__Videvo.mov75480.webm' type='video/mp4' />
               Your browser does not support the video tag.
             </video>
