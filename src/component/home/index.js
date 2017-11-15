9481d45aa1df10871ada41c0a3e3dbe8a39dfd62
Matthew Parker <cadburylion@outlook.com>
2017-11-14 22:21:53 -0800

    Added route functionality to home icons

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -2,20 +2,34 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Header from '../header/index.js'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import FontAwesome from 'react-fontawesome'
 import ThemeIcon from '../theme-icon/index.js'
+import * as route from '../../action/route.js'
 
 import './styles.scss'
 
 class Home extends React.Component{
   render(){
-    console.log('lightTheme: ', this.props.lightTheme)
     return(
       <div className={`home-main ${this.props.lightTheme ? 'home-main-light' : ''}`}>
 
         <div className='home-cover'></div>
 
         <div className='home-intro-text'>{`Hi, I'm Matthew`}</div>
+
+        <Link to='/' onClick={this.props.routeToPortfolio} className='home-portfolio-icon'>
+          <FontAwesome name='file-code-o' />
+        </Link>
+
+        <Link to='/about' onClick={this.props.routeToAbout} className='home-about-icon'>
+          <FontAwesome name='user-circle-o' />
+        </Link>
+
+        <Link to='/contact' onClick={this.props.routeToContact} className='home-contact-icon'>
+          <FontAwesome name='envelope-o' />
+        </Link>
+
 
         <ThemeIcon
           className={`theme-toggle-icon ${this.props.lightTheme ? 'theme-toggle-icon-light' : ''}`}
@@ -28,11 +42,13 @@ class Home extends React.Component{
 }
 
 let mapStateToProps = (state) => ({
-  lightTheme: state.lightTheme
+  lightTheme: state.lightTheme,
 })
 
 let mapDispatchToProps = (dispatch) => ({
-
+  routeToAbout: () => dispatch(route.switchRoute('/about')),
+  routeToPortfolio: () => dispatch(route.switchRoute('/')),
+  routeToContact: () => dispatch(route.switchRoute('/contact')),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- src/reducer/route.js
+++ src/reducer/route.js
@@ -1,9 +1,6 @@
 export default (state='', {type, payload}) => {
   switch(type){
   case 'SWITCH_ROUTE': return payload
-  case 'ABOUT': return '/about'
-  case 'PORTFOLIO': return '/portfolio'
-  case 'CONTACT': return '/contact'
   default: return state
   }
 }
