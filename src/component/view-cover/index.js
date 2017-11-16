1a5389197d1faf65cc260c6d29266782a4f8f658
Matthew Parker <cadburylion@outlook.com>
2017-11-15 20:54:25 -0800

    Janky code to detect browser and set either background image or background video depending

--- src/component/view-cover/index.js
+++ src/component/view-cover/index.js
@@ -8,48 +8,44 @@ import './style.scss'
 class ViewCover extends React.Component{
   constructor(props){
     super(props)
+    this.state={
+      playVideo: false,
+    }
     this.handleCover = this.handleCover.bind(this)
+    this.detectBrowser = this.detectBrowser.bind(this)
   }
 
   handleCover(){
     this.props.handleCover('COVER_TOGGLE')
   }
-  //
-  // browserSpecificCode(){
-  //   //Check if browser is IE
-  //   if (navigator.userAgent.search("MSIE") & gt; = 0) {
-  //       // insert conditional IE code here
-  //   }
-  //   //Check if browser is Chrome
-  //   else if (navigator.userAgent.search("Chrome") & gt; = 0) {
-  //       // insert conditional Chrome code here
-  //   }
-  //   //Check if browser is Firefox
-  //   else if (navigator.userAgent.search("Firefox") & gt; = 0) {
-  //       // insert conditional Firefox Code here
-  //   }
-  //   //Check if browser is Safari
-  //   else if (navigator.userAgent.search("Safari") & gt; = 0 & amp; & amp; navigator.userAgent.search("Chrome") & lt; 0) {
-  //       // insert conditional Safari code here
-  //   }
-  //   //Check if browser is Opera
-  //   else if (navigator.userAgent.search("Opera") & gt; = 0) {
-  //       // insert conditional Opera code here
-  //   }
-  //
-  // }
+
+  componentWillMount(){
+    let video = this.detectBrowser()
+
+    video === 'chrome' || video === 'firefox' ? this.setState({playVideo: true}) : undefined
+  }
+
+  detectBrowser(){
+    let browser
+    let browserMatch = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i)
+    navigator.userAgent.match(/Edge/i) || navigator.userAgent.match(/Trident.*rv[ :]*11\./i) ? browser = 'msie'
+    : browser = browserMatch[1].toLowerCase()
+
+    return browser
+  }
+
+
 
   render(){
-
-    console.log(navigator.userAgent.search('Firefox') > 0)
 
     let arrowIconClass = (this.props.coverOpen && this.props.lightTheme) ? 'view-cover-toggle-icon-open-light'
     : (this.props.coverOpen && !this.props.lightTheme) ? 'view-cover-toggle-icon-open-dark'
     : (!this.props.coverOpen && this.props.lightTheme) ? 'view-cover-toggle-icon-closed-light'
     : (!this.props.coverOpen && !this.props.lightTheme) ? 'view-cover-toggle-icon-closed-dark'
     : ''
+
     return (
-      <div className={`view-cover-main ${this.props.coverOpen ? 'view-cover-main-open' : ''}`} onClick={this.handleCover} >
+      <div className={`view-cover-main ${this.props.coverOpen ? 'view-cover-main-open' : ''} ${!this.state.playVideo ? 'bg-image' : ''}`} onClick={this.handleCover}>
 
 
         <div className='view-cover-content' >
@@ -63,21 +59,20 @@ class ViewCover extends React.Component{
           <p className='view-cover-p3'>Beautiful</p>
           <p className='view-cover-p4'>things</p>
 
-          {!this.props.lightTheme ?
-
-              <video className='background-video' loop muted autoPlay alt='video loop of stars'>
-                <source src='https://d1235ca2z646oc.cloudfront.net/videos/processed/255/MadeInLadakh-HD_2.mp4.mp4' type='video/mp4' />
-                Your browser does not support the video tag.
-              </video>
-
-            : undefined
-          }
-          {this.props.lightTheme ?
-            <video className='background-video' loop muted autoPlay alt='video loop of clouds'>
-              <source src='https://www.videvo.net/videvo_files/converted/2014_06/preview/Blue_Sky_and_Clouds_Timelapse_0892__Videvo.mov75480.webm' type='video/mp4' />
+          {!this.props.lightTheme && this.state.playVideo ?
+            <video className='background-video' loop muted autoPlay alt='video loop of stars'>
+              <source src='https://d1235ca2z646oc.cloudfront.net/videos/processed/255/MadeInLadakh-HD_2.mp4.mp4' type='video/mp4' />
               Your browser does not support the video tag.
             </video>
-            : undefined
+          : undefined
+          }
+
+          {this.props.lightTheme && this.state.playVideo ?
+            <video className='background-video' loop muted autoPlay alt='video loop of clouds'>
+             <source src='https://www.videvo.net/videvo_files/converted/2014_06/preview/Blue_Sky_and_Clouds_Timelapse_0892__Videvo.mov75480.webm' type='video/mp4' />
+             Your browser does not support the video tag.
+           </video>
+          : undefined
           }
 
         </div>
