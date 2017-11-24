87bac09f34fe5ec32b0673581ddbc2e7b4cf97ca
Matthew Parker <cadburylion@outlook.com>
2017-11-24 14:26:01 -0800

    Added new project template and associated styles

--- src/component/portfolio/index.js
+++ src/component/portfolio/index.js
@@ -1,6 +1,7 @@
 import './styles.scss'
 import React from 'react'
 import {scroller} from 'react-scroll'
+import Project from '../project/index.js'
 import ViewCover from '../view-cover/index.js'
 
 class Portfolio extends React.Component{
@@ -17,61 +18,64 @@ class Portfolio extends React.Component{
     }
 
     return(
-      <main className='main'>
-
-        <section className='featured-work'>
-          <h2 id='portfolio'></h2>
-
-          <ul className='projects'>
-
-
-            <li className='seattle-reads'>
-              <a href='https://seattle-library-checkouts.herokuapp.com/' target='_blank'>
-                <h3 className='title'> Seattle Reads </h3>
-              </a>
-              <h4>
-                  <a className='github-icon' href='https://github.com/Cadburylion/seattle-library-checkouts' target='_#'>
-                    <i className="fa fa-github" aria-hidden="true"></i>
-                  </a>
-                    React, JavaScript, HTML5, CSS3, Sass
-              </h4>
-            </li>
-
-
-            <li className='gourmet-swap'>
-              <a href='https://gourmet-swap-frontend.herokuapp.com/landing' target='_blank'>
-                <h3 className='title'> Gourmet Swap </h3>
-              </a>
-              <h4>
-                  <a className='github-icon' href='https://github.com/amandakoster/GourmetSwap-frontend' target='_#'>
-                    <i className="fa fa-github" aria-hidden="true"></i>
-                  </a>
-                  <a className='github-icon' href='https://github.com/0smium/GourmetSwap-backend' target='_#'>
-                    <i className="fa fa-github" aria-hidden="true"></i>
-                  </a>
-                    HTML5, Sass, MongoDB, JavaScript, React, 401 Project
-              </h4>
-            </li>
-
-
-
-            <li className='guardians'>
-              <a href='https://cadburylion.github.io/' target='_blank'>
-                <h3 className='title'> Guardians of the Goats </h3>
-              </a>
-              <h4>
-                  <a className='github-icon' href='https://github.com/izzybaer/guardians-of-the-goats' target='_#'>
-                    <i className="fa fa-github" aria-hidden="true"></i>
-                  </a>
-                    HTML5, CSS3, JavaScript, 201 Project
-              </h4>
-            </li>
-
-          </ul>
-        </section>
-      </main>
+      <div className='portfolio-container'>
+        <div className='project-sr'>
+          <Project />
+        </div>
+      </div>
     )
   }
 }
+
+// <section className='featured-work'>
+//   <h2 id='portfolio'></h2>
+//
+//   <ul className='projects'>
+//
+//
+//     <li className='seattle-reads'>
+//       <a href='https://seattle-library-checkouts.herokuapp.com/' target='_blank'>
+//         <h3 className='title'> Seattle Reads </h3>
+//       </a>
+//       <h4>
+//           <a className='github-icon' href='https://github.com/Cadburylion/seattle-library-checkouts' target='_#'>
+//             <i className="fa fa-github" aria-hidden="true"></i>
+//           </a>
+//             React, JavaScript, HTML5, CSS3, Sass
+//       </h4>
+//     </li>
+//
+//
+//     <li className='gourmet-swap'>
+//       <a href='https://gourmet-swap-frontend.herokuapp.com/landing' target='_blank'>
+//         <h3 className='title'> Gourmet Swap </h3>
+//       </a>
+//       <h4>
+//           <a className='github-icon' href='https://github.com/amandakoster/GourmetSwap-frontend' target='_#'>
+//             <i className="fa fa-github" aria-hidden="true"></i>
+//           </a>
+//           <a className='github-icon' href='https://github.com/0smium/GourmetSwap-backend' target='_#'>
+//             <i className="fa fa-github" aria-hidden="true"></i>
+//           </a>
+//             HTML5, Sass, MongoDB, JavaScript, React, 401 Project
+//       </h4>
+//     </li>
+//
+//
+//
+//     <li className='guardians'>
+//       <a href='https://cadburylion.github.io/' target='_blank'>
+//         <h3 className='title'> Guardians of the Goats </h3>
+//       </a>
+//       <h4>
+//           <a className='github-icon' href='https://github.com/izzybaer/guardians-of-the-goats' target='_#'>
+//             <i className="fa fa-github" aria-hidden="true"></i>
+//           </a>
+//             HTML5, CSS3, JavaScript, 201 Project
+//       </h4>
+//     </li>
+//
+//   </ul>
+// </section>
 
 export default Portfolio
--- src/component/project/index.js
+++ src/component/project/index.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import './style.scss'
 
 const Project = (props) => {
   return (
-    <li className={props.title}>
-      <a href={props.projectURL} target='_blank'>
-        <h3 className='title'> Seattle Reads </h3>
-      </a>
-      <h4>
-          <a className='github-icon' href={props.projectGitHubURL} target='_#'>
-            <i className="fa fa-github" aria-hidden="true"></i>
+    <div className={'project-main'}>
+
+      <div className={'project-description'}>
+        Seattle Reads is a web app that makes it easy to see what items are being checked out of the Seattle public library system and find popular new reads.
+      </div>
+
+      <div className={'project-portrait'}>
+        <div className={'project-picture'}>
+        <a href='https://seattle-library-checkouts.herokuapp.com/' target='_#'>
+          <div className={'project-logo-name'}>
+            SR
+          </div>
           </a>
-            React, JavaScript, HTML5, CSS3, Sass
-      </h4>
-    </li>
+        </div>
+
+        <div className={'project-banner'}>
+          <div className={'project-name'}>
+            Seattle Reads
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
