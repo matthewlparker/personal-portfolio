79c4121ba145418d63db358d0b25073ff3563edf
Matthew Parker <cadburylion@outlook.com>
2017-11-24 17:26:23 -0800

    Added dynamic content and styles to projects

--- src/component/portfolio/index.js
+++ src/component/portfolio/index.js
@@ -23,6 +23,8 @@ class Portfolio extends React.Component{
           <Project
             logoName={'SR'}
             title={'Seattle Reads'}
+            projectColor={'rgba(55, 121, 182, .4)'}
+            url={'https://seattle-library-checkouts.herokuapp.com/'}
             description={'A web app that makes it easy to see what items are being checked out of the Seattle public library system, and find popular new reads.'}
           />
         </div>
@@ -31,6 +33,8 @@ class Portfolio extends React.Component{
           <Project
             logoName={'GS'}
             title={'Gourmet Swap'}
+            projectColor={'rgba(206, 64, 43, .4)'}
+            url={'https://gourmet-swap-frontend.herokuapp.com/'}
             description={'A full-stack application built during my 401 class at Code Fellows as a prototype for a gourmet meal sharing startup.'}
           />
         </div>
@@ -39,6 +43,8 @@ class Portfolio extends React.Component{
           <Project
             logoName={'GoG'}
             title={'Guardians'}
+            url={'https://cadburylion.github.io/'}
+            projectColor={'rgba(54, 54, 54, .4)'}
             description={'My first project at Code Fellows, Guardians of the Goats is a front-end mockup that allows users to gain access to information about caring for goats, to advertise a goat that is need of help, and to make yourself available to offer that help.'}
           />
         </div>
