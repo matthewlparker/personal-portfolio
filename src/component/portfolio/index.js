296b495efe3cdc6c74148771772cfb50c87d48bc
Matthew Parker <cadburylion@outlook.com>
2019-08-04 19:45:16 -0700

    Added Lionly to portfolio; removed GoG from portfolio

--- src/component/portfolio/index.js
+++ src/component/portfolio/index.js
@@ -1,64 +1,67 @@
 import './styles.scss'
 import React from 'react'
-import {scroller} from 'react-scroll'
-import {connect} from 'react-redux'
+import { scroller } from 'react-scroll'
+import { connect } from 'react-redux'
 import Project from '../project/index.js'
 import ViewCover from '../view-cover/index.js'
 import * as setRoute from '../../action/route.js'
 import * as viewActions from '../../action/viewActions.js'
 
-class Portfolio extends React.Component{
-  constructor(props){
+class Portfolio extends React.Component {
+  constructor(props) {
     super(props)
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.props.shareRoute('/portfolio')
   }
 
-  componentDidMount(){
-
+  componentDidMount() {
     this.props.pageActive(true)
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.props.pageActive(false)
   }
 
   render() {
-
-    return(
-      <div className='portfolio-container'>
-        <div className='project sr'>
+    return (
+      <div className="portfolio-container">
+        <div className="project one">
+          <Project
+            logoName={'LY'}
+            title={'Lionly'}
+            url={'https://lion.ly'}
+            projectColor={'rgba(54, 54, 54, .4)'}
+            description={
+              'A URL shortener with optional user signup & signin functionality utilizing industry standard security tools and practices.'
+            }
+            skills={'react, node, mongodb, oauth, jwt, bcrypt'}
+          />
+        </div>
+        <div className="project two">
           <Project
             logoName={'SR'}
             title={'Seattle Reads'}
             projectColor={'rgba(55, 121, 182, .4)'}
             url={'https://seattle-library-checkouts.herokuapp.com/'}
-            description={'A web app that makes it easy to see what items are being checked out of the Seattle public library system, and find popular new reads.'}
+            description={
+              'A web app that makes it easy to see what items are being checked out of the Seattle public library system, and find popular new reads.'
+            }
             skills={'javascript, react, redux, html5, sass, git'}
           />
         </div>
 
-        <div className='project gs'>
+        <div className="project three">
           <Project
             logoName={'GS'}
             title={'Gourmet Swap'}
             projectColor={'rgba(206, 64, 43, .4)'}
             url={'https://gourmet-swap-frontend.herokuapp.com/'}
-            description={'A full-stack application built during my 401 class at Code Fellows as a prototype for a gourmet meal sharing startup.'}
+            description={
+              'A full-stack application built during my 401 class at Code Fellows as a prototype for a gourmet meal sharing startup.'
+            }
             skills={'javascript, react, redux, mongodb, git'}
-          />
-        </div>
-
-        <div className='project gog'>
-          <Project
-            logoName={'GoG'}
-            title={'Guardians of the Goats'}
-            url={'https://cadburylion.github.io/'}
-            projectColor={'rgba(54, 54, 54, .4)'}
-            description={'My first project at Code Fellows, Guardians of the Goats is a front-end mockup of a goat-care resource site. Goats in need deserve a Guardian indeed.'}
-            skills={'javascript, html5, css3, git'}
           />
         </div>
       </div>
@@ -66,11 +69,14 @@ class Portfolio extends React.Component{
   }
 }
 
-let mapStateToProps = (state) => ({})
+let mapStateToProps = state => ({})
 
-let mapDispatchToProps = (dispatch) => ({
-  shareRoute: (route) => dispatch(setRoute.route(route)),
-  pageActive: (bool) => dispatch(viewActions.pageActive(bool)),
+let mapDispatchToProps = dispatch => ({
+  shareRoute: route => dispatch(setRoute.route(route)),
+  pageActive: bool => dispatch(viewActions.pageActive(bool)),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Portfolio)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Portfolio)
--- src/component/project/index.js
+++ src/component/project/index.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import './style.scss'
 
-const Project = (props) => {
+const Project = props => {
   let projectColor = {
     backgroundColor: props.projectColor,
   }
   return (
-    <a href={props.url} target='_#' className={'project-main'}>
-
+    <a href={props.url} target="_#" className={'project-main'}>
       <div className={'project-card'}>
-
-        <div className='front box'>
+        <div className="front box">
           <div className={'title'}>{props.title}</div>
         </div>
 
-        <div className='back box'>
+        <div className="back box">
           <div className={'description'}>{props.description}</div>
         </div>
-
       </div>
-
     </a>
   )
 }
